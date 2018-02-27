49bd2b49ca28a7afe2b3b6ea8de2e156053e59cb
hugo27rocha <36748053+hugo27rocha@users.noreply.github.com>
2018-02-27 22:49:31 +0000

    Update db_create.php

--- db_create.php
+++ db_create.php
@@ -3,32 +3,7 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:fcul.database.windows.net,1433; Database = app", "Elementos", "{Fcul2018!}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql1 = CREATE TABLE `admin_table` (
-  	`admin_id` int(5) NOT NULL,
-  	`username` varchar(255) NOT NULL,
-  	`password` varchar(255) NOT NULL,
-  	`email` varchar(255) NOT NULL
-	);
-
-   $sql2 = CREATE TABLE `alunos_table` (
-    `alunos_id` int(5) NOT NULL,
-    `username` varchar(255) NOT NULL,
-    `password` varchar(255) NOT NULL,
-    `email` varchar(255) NOT NULL
-  );
-
-  $sql3 =CREATE TABLE `prof_table` (
-  `prof_id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
-);
-
-  $conn->query($sql);
-
-  $conn->query($sql2);
-
-  $conn->query($sql3);
+    
 
 }
 catch (PDOException $e) {
